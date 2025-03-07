421a5cfed759e2a48e09f8685d0c182291e730bf
t123n123 <spineim22@gmail.com>
2025-03-07 21:53:37 +0100

    wood

--- src/client/java/obscurus/obscurus/datagen/ItemTagProvider.java
+++ src/client/java/obscurus/obscurus/datagen/ItemTagProvider.java
@@ -5,6 +5,8 @@ import java.util.concurrent.CompletableFuture;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
+import net.minecraft.registry.tag.ItemTags;
+import obscurus.obscurus.ObscurusBlocks;
 
 public class ItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
@@ -14,7 +16,13 @@ public class ItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(WrapperLookup wrapperLookup) {
-        // add items to tags
+
+        getOrCreateTagBuilder(ItemTags.LOGS_THAT_BURN)
+            .add(ObscurusBlocks.OBSCURUS_LOG.asItem())
+            .add(ObscurusBlocks.OBSCURUS_WOOD.asItem());
+
+        getOrCreateTagBuilder(ItemTags.PLANKS)
+            .add(ObscurusBlocks.OBSCURUS_PLANKS.asItem());
     }
     
 }
